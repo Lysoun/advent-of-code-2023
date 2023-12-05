867e6d34a8bd6049c249dbf956e0e077f0a64fd3
Élise Rubio <elise.rubio@4sh.fr>
2023-12-05 22:52:38 +0100

    Reorganize project to use gradle

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.20"
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+repositories {
+    mavenCentral()
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "advent-of-code-2023"
+include(
+    "day1",
+    "day2",
+    "day3",
+    "day4",
+)
